4fe42f7b951968eb00c4c194e06e509cf0861666
David Agudelo <agudeloocampodavid@gmail.com>
2025-10-29 13:57:45 -0500

    changes search

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
+import { lusitana } from './ui/fonts';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-gray-50 font-sans">
-
-
-        <main>{children}</main>
-
-        {/* Example: footer */}
-        <footer className="p-4 text-center text-gray-600">© 2025 Acme</footer>
-      </body>
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 // app/page.tsx
-
 import Link from 'next/link';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { Lusitana } from 'next/font/google';
+import { lusitana } from './ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -11,16 +13,8 @@ export default function Page() {
 
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-100 px-6 py-10 md:w-2/5 md:px-20">
-          <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
-            <div
-  className="relative w-0 h-0 border-l-15 border-r-15 border-b-26 border-l-transparent border-r-transparent border-b-black"
-/>
-            <strong>Welcome to Acme.</strong> This is the example dashboard app built using{' '}
-            <a href="https://nextjs.org/" className="text-blue-500 hover:underline">
-              Next.js
-            </a>
-            .
-          </p>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}> 
+            <strong>Welcome to Acme.</strong> This is the example dashboard app built using</p>
           <Link
             href="/login"
             className="flex items-center gap-2 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
@@ -28,9 +22,27 @@ export default function Page() {
             <span>Log in</span>
             <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
-        </div>
+          </div>
+        
 
-      </div>
+          <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+           <Image
+          src="/computer.jpg"
+          width={500}
+          height={760}
+          className="hidden md:block"
+          alt='image of a camera'
+          />
+          
+          <Image 
+          src="/planet.jpg"
+          width={560}
+          height={620}
+          className="block md:hidden"
+          alt='image of the sky'
+          />
+          </div>
+          </div>
     </main>
   );
 }
